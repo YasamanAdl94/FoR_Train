9ad5f8432d38ed906f59fac74ff93f126eacf140
yahmadia <yahmadiadli@ryerson.ca>
2023-12-24 15:25:46 -0500

    Chnage frozen layers

--- Main.py
+++ Main.py
@@ -11,7 +11,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import regularizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow_io as tfio  # Import tensorflow-io for signal processing
+
+
 
 
 
@@ -27,6 +31,7 @@ epochs = 10
 img_height = 224
 img_width = 224
 
+
 print("\033[1mCreating training and validation datasets:\033[0m")
 training_ds = tf.keras.utils.image_dataset_from_directory(
     training_dir,
@@ -38,6 +43,22 @@ training_ds = tf.keras.utils.image_dataset_from_directory(
     crop_to_aspect_ratio=True,
     label_mode='binary',
     class_names=['fake', 'real']
+)
+
+
+def apply_audio_augmentation(image, label):
+    # Apply time masking
+    spectrogram = tfio.audio.time_mask(image, param=10)  # Adjust 'param' as needed
+
+    # Apply frequency masking
+    spectrogram = tfio.audio.freq_mask(image, param=5)  # Adjust 'param' as needed
+
+    return image, label
+
+
+augmented_training_ds = training_ds.map(
+    apply_audio_augmentation,
+    num_parallel_calls=tf.data.AUTOTUNE
 )
 
 validation_ds = tf.keras.utils.image_dataset_from_directory(
@@ -66,15 +87,25 @@ test_ds = tf.keras.utils.image_dataset_from_directory(
 class_names = training_ds.class_names
 print("\nNames of", str(len(class_names)), "classes:", class_names)
 
-# Build and compile model
-model = keras.Sequential()
-model.add(keras.applications.resnet50.ResNet50(
+
+# Build the model
+base_model = keras.applications.ResNet50(
     include_top=False,
     weights="imagenet",
     pooling="avg"
-))
-model.add(keras.layers.Dense(1, activation="sigmoid"))
-model.layers[0].trainable = False
+)
+
+# Freeze layers except the last few
+for layer in base_model.layers[:-7]:  # Unfreeze the last 5 layers for example
+    layer.trainable = False
+
+# Create your model on top of the base model
+model = keras.Sequential([
+    base_model,
+    keras.layers.Dense(1, activation='sigmoid')
+])
+#model.add(keras.layers.Dense(1, activation="sigmoid", kernel_regularizer=regularizers.l1(0.01)))
+#model.layers[0].trainable = True
 
 model.compile(
     optimizer="adam",
@@ -97,17 +128,9 @@ steps_per_epoch = len(training_ds) / epochs
 
 t0 = time.time()
 
-model.fit(
-    training_ds,
-    steps_per_epoch=steps_per_epoch,
-    validation_data=validation_ds,
-    validation_steps=1,
-    epochs=epochs
-)
-
 # Model training
 history = model.fit(
-    training_ds,
+    augmented_training_ds,
     steps_per_epoch=steps_per_epoch,
     validation_data=validation_ds,
     validation_steps=1,
@@ -117,6 +140,8 @@ history = model.fit(
 # Access training loss and accuracy
 train_loss = history.history['loss']
 train_accuracy = history.history['binary_accuracy']
+val_loss = history.history['val_loss']  # Validation loss
+val_accuracy = history.history['val_binary_accuracy']  # Validation accuracy
 
 # Print or use the values as needed
 print("Training Loss per epoch:", train_loss)
@@ -131,6 +156,8 @@ results = model.evaluate(
     test_ds,
     return_dict=True
 )
+test_accuracy = results['binary_accuracy']
+print("test accuracy", test_accuracy )
 
 def f1score(p, r):
     f1 = 2 / ((1 / p) + (1 / r))
@@ -149,6 +176,8 @@ tp, fp = results['true_positives'], results['false_positives']
 fn, tn = results['false_negatives'], results['true_negatives']
 cmx = np.array([[tp, fp], [fn, tn]], np.int32)
 
+model.save("W:/workdir/Models/model2.h5")
+
 cmx_plot = sns.heatmap(
     cmx / np.sum(cmx),
     cmap='Blues',
@@ -161,3 +190,26 @@ cmx_plot = sns.heatmap(
     yticklabels=['Spoof (1)', 'Real (0)']
 )
 cmx_plot.set(xlabel="Actual", ylabel="Predicted")
+
+
+plt.figure(figsize=(12, 4))
+
+# Plotting training loss
+plt.subplot(1, 2, 1)
+plt.plot(train_loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+# Plotting training accuracy
+plt.subplot(1, 2, 2)
+plt.plot(train_accuracy, label='Training Accuracy')
+plt.plot(val_accuracy, label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout()
+plt.savefig("W:/workdir/Plots/plot.png")
+plt.show()
