f22da33b24fdd8ce3d0f4c3acbafb2e781ecb3d6
yahmadia <yahmadiadli@ryerson.ca>
2023-12-25 13:34:06 -0500

    test script

--- test.py
+++ test.py
@@ -18,60 +18,21 @@ import tensorflow_io as tfio  # Import tensorflow-io for signal processing
 
 
 
-
-training_dir = pathlib.Path("W:\\workdir\\train")
-test_dir = pathlib.Path("W:\\workdir\\test")
+saved_model_path = "W:/workdir/Models/model2.h5"
+loaded_model = keras.models.load_model(saved_model_path)
+#training_dir = pathlib.Path("W:\\workdir\\train")
+test_dir = pathlib.Path("W:\\workdir3\\test")
 
 test_count = len(list(test_dir.glob('*/*.png')))
-train_count = len(list(training_dir.glob('*/*.png')))
-validation_split = test_count / train_count
+#train_count = len(list(training_dir.glob('*/*.png')))
+#validation_split = test_count / train_count
 # Define parameters and create datasets
 batch_size = 100
-epochs = 20
+#epochs = 20
 img_height = 224
 img_width = 224
 
 
-print("\033[1mCreating training and validation datasets:\033[0m")
-training_ds = tf.keras.utils.image_dataset_from_directory(
-    training_dir,
-    validation_split=validation_split,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-    crop_to_aspect_ratio=True,
-    label_mode='binary',
-    class_names=['fake', 'real']
-)
-
-
-def apply_audio_augmentation(image, label):
-    # Apply time masking
-    spectrogram = tfio.audio.time_mask(image, param=10)  # Adjust 'param' as needed
-
-    # Apply frequency masking
-    spectrogram = tfio.audio.freq_mask(image, param=5)  # Adjust 'param' as needed
-
-    return image, label
-
-
-augmented_training_ds = training_ds.map(
-    apply_audio_augmentation,
-    num_parallel_calls=tf.data.AUTOTUNE
-)
-
-validation_ds = tf.keras.utils.image_dataset_from_directory(
-    training_dir,
-    validation_split=validation_split,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-    crop_to_aspect_ratio=True,
-    label_mode='binary',
-    class_names=['fake', 'real']
-)
 
 print("\n\033[1mCreating test dataset:\033[0m")
 test_ds = tf.keras.utils.image_dataset_from_directory(
@@ -84,11 +45,9 @@ test_ds = tf.keras.utils.image_dataset_from_directory(
     class_names=['fake', 'real']
 )
 
-class_names = training_ds.class_names
-print("\nNames of", str(len(class_names)), "classes:", class_names)
-
 
 # Build the model
+'''
 base_model = keras.applications.ResNet50(
     include_top=False,
     weights="imagenet",
@@ -128,6 +87,8 @@ steps_per_epoch = len(training_ds) / epochs
 
 t0 = time.time()
 
+saved_model_path = "W:/workdir/Models/model2.h5"
+loaded_model = keras.models.load_model(saved_model_path)
 # Model training
 history = model.fit(
     augmented_training_ds,
@@ -150,14 +111,14 @@ print("Training Accuracy per epoch:", train_accuracy)
 
 t1 = time.time()
 dt = (t1 - t0)
-
+'''
 # Model evaluation
-results = model.evaluate(
+results = loaded_model.evaluate(
     test_ds,
     return_dict=True
 )
 test_accuracy = results['binary_accuracy']
-print("test accuracy", test_accuracy )
+print("test accuracy on ADD dataset", test_accuracy )
 
 def f1score(p, r):
     f1 = 2 / ((1 / p) + (1 / r))
@@ -169,14 +130,14 @@ for i in results:
     print(i + ": " + str(results[i]))
 print("-" * 70)
 print("F1 Score: " + str(f1score(results['precision'], results['recall'])))
-print("Time to train: ", dt)
+#print("Time to train: ", dt)
 print("-" * 70)
 
 tp, fp = results['true_positives'], results['false_positives']
 fn, tn = results['false_negatives'], results['true_negatives']
 cmx = np.array([[tp, fp], [fn, tn]], np.int32)
 
-model.save("W:/workdir/Models/model4.h5")
+#model.save("W:/workdir/Models/model4.h5")
 
 cmx_plot = sns.heatmap(
     cmx / np.sum(cmx),
@@ -193,19 +154,19 @@ cmx_plot.set(xlabel="Actual", ylabel="Predicted")
 
 
 plt.figure(figsize=(12, 4))
-
+'''
 # Plotting training loss
 plt.subplot(1, 2, 1)
-plt.plot(train_loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
+#plt.plot(train_loss, label='Training Loss')
+#plt.plot(val_loss, label='Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 
 # Plotting training accuracy
 plt.subplot(1, 2, 2)
-plt.plot(train_accuracy, label='Training Accuracy')
-plt.plot(val_accuracy, label='Validation Accuracy')
+#plt.plot(train_accuracy, label='Training Accuracy')
+#plt.plot(val_accuracy, label='Validation Accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
@@ -213,3 +174,4 @@ plt.legend()
 plt.tight_layout()
 plt.savefig("W:/workdir/Plots/plot4.png")
 plt.show()
+'''
